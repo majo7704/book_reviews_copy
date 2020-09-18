e755821c66d43a203af64f11f66433228a83b031
j m <jm@js-macbook-pro.home>
2020-09-18 08:38:23 +0200

    implements create session action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+  def new 
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if(user.present? && user.authenticate(params[:password]))
+      session[:user_id] = user.id
+      redirect_to account_reviews_path
+    else
+      flash[:alert] = 'Email or password were invalid. Plaese try again'
+      render 'new'
+  end  
 end
