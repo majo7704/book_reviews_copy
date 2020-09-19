7624c32b4bb50389ed29341e045bf9fe3befb281
j m <jm@js-macbook-pro.home>
2020-09-19 22:16:18 +0200

    implements before action filter to protect Account resources

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,6 +1,13 @@
 class AccountController < ApplicationController
-def reviews
-  @reviews = Review.all
-end
+  before_action :ensure_authenticated
+  def reviews
+    @reviews = Review.all
+  end
+
+  def ensure_authenticated
+    unless(logged_in?)
+      redirect_to login_path
+    end
+  end
 
 end
