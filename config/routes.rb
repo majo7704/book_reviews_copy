3879d4b8d211555aa9dc6090327bc191e86ce3bf
j m <jm@js-macbook-pro.home>
2020-09-19 18:13:07 +0200

    implements loggout functionality'

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :users do
     resources :bookmarks
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
   get 'account/reviews'
 
   get 'about', to:'about#me'
+
+  get 'login', to:'sessions#new'
+
+  get 'signup', to:'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  
   test 'email is lowercase before validation' do
     user = User.new email:'mE@EpfL.ch', password:'password'
     user.save!
